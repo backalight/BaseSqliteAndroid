cc9e508e75e09793fbe750fee66bd7358dac4be0
backalight <antoine.soros@orange.fr>
2018-10-22 14:38:24 +0200

    sqlitAndroid

---
+++ app/src/main/java/fr/formation/basesqliteandroid/DbInit.java
@@ -0,0 +1,33 @@
+package fr.formation.basesqliteandroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public abstract class DbInit extends SQLiteOpenHelper {
+
+    public DbInit(Context ctxt, String dbName, int version) {
+        super(ctxt, dbName, null, version);
+    }
+
+
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE contacts (" +
+                " id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL" +
+                ", name TEXT NOT NULL" +
+                ", tel TEXT" +
+                ")";
+        db.execSQL(sql);
+    }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
